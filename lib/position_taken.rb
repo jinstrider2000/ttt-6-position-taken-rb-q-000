eae36a6b0f8fb1d7e23f89a437ad8a1cd30561bc
jinstrider2000 <efrainperezjr@live.com>
2016-05-09 20:54:40 -0400

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,10 @@
 # code your #position_taken? method here!
+def position_taken?(array,pos)
+  
+  if(array[pos] == " " || array[pos] == "" || array[pos] == nil)
+    false
+  else
+    true
+  end
+  
+end
